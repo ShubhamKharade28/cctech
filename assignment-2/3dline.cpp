fa6c4b3fab9f72c78dd353ce65fab73f6eea9177
Shubham Kharade <shubham.kharade28@gmail.com>
2025-03-19 07:33:03 +0000

    fixed display issue

---
+++ assignment-2/3dline.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include "gnuplot-iostream.h"
+using namespace std;
+
+class Point {
+    public:
+    double x,y,z;
+
+    Point(){
+        this->x = 0;
+        this->y = 0;
+        this->z = 0;
+    }
+
+    Point(double x, double y, double z){
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+};
+
+vector<Point> generatePoints(Point p1, Point p2){
+    vector<Point> points;
+    
+    double dx = (p2.x - p1.x) / 9.0;  
+    double dy = (p2.y - p1.y) / 9.0;
+    double dz = (p2.z - p1.z) / 9.0;
+
+    for (int i = 0; i < 10; i++) {
+        double x = p1.x + i * dx;
+        double y = p1.y + i * dy;
+        double z = p1.z + i * dz;
+        Point p(x,y,z);
+        points.push_back(p);
+    }
+
+    return points;
+}
+
+void print(vector<Point>& points){
+    cout << "Points: "<<endl;
+    for(auto& point : points){
+        cout << point.x << "\t" << point.y << "\t" << point.y << endl;
+    }
+    cout << endl;
+}
+
+vector<tuple<double,double,double>> pointToTuple(vector<Point>& points){
+    vector<tuple<double,double,double>> res;
+    for(auto& point : points){
+        res.push_back({point.x, point.y, point.z});
+    }
+    return res;
+}
+
+int main(){
+    // input start and end coordinates
+    double x1,x2,y1,y2,z1,z2;
+    cout << "Enter start coords: ";
+    cin >> x1 >> y1 >> z1;
+    cout << "Enter end coords: ";
+    cin >> x2 >> y2 >> z2;
+
+    Point p1(x1,y1,z1);
+    Point p2(x2,y2,z2);
+
+    // generate points from start to end to plot
+    vector<Point> points = generatePoints(p1,p2);
+    vector<tuple<double,double,double>> coords = pointToTuple(points);
+
+    // setup gnuplot
+    Gnuplot gp;
+    gp << "set terminal wxt\n";
+    // gp << "set terminal dumb\n";
+
+    // Set Gnuplot options for 3D plotting
+    gp << "set xlabel 'X'\n";          // Label X-axis
+    gp << "set ylabel 'Y'\n";          // Label Y-axis
+    gp << "set zlabel 'Z'\n";          // Label Z-axis
+    gp << "set title '3D Line Plot'\n"; // Title
+    gp << "splot '-' with lines\n";    // Plot data as a 3D line
+
+    gp.send1d(coords);
+}
