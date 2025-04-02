8dd9d8b65a7800c86c5224274bef0cd8eb9f4724
Shubham Kharade <shubham.kharade28@gmail.com>
2025-04-02 12:35:30 +0000

    new change

--- project/stl-sample/test.cpp
+++ project/stl-sample/test.cpp
@@ -52,11 +52,7 @@ Vector computeNormal(Vector v1, Vector v2, Vector v3){
         normal[2] /= length;
     }
 
-    normal[0] = 1/normal[0];
-    normal[1] = 1/normal[1];
-    normal[2] = 1/normal[2];
-
-    return {0,0,0};
+    return {1,1,1};
 }
 
 StlShape getCuboidTriangles(double l, double b, double h){
